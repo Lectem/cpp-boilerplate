9061d50bdcaf9b46892a89a967e47647e6457520
Lectem <lectem@gmail.com>
2017-08-13 23:39:03 +0200

    Fix batch script missing submodule

--- CreateBoilerPlate.bat
+++ CreateBoilerPlate.bat
@@ -6,4 +6,5 @@ git commit -m"initial commit"
 cd external
 git submodule add --depth 1 -- https://github.com/fmtlib/fmt.git
 git submodule add --depth 1 -- https://github.com/gabime/spdlog.git
+git submodule add --depth 1 -- https://github.com/onqtam/doctest.git
 cd ..
