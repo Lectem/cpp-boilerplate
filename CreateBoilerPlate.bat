e35e6de4ec3aaabea9ac702655edb3fb7729fdea
Clément Gregoire <cgregoire@asobostudio.com>
2017-08-23 14:28:24 +0200

    Remove empty external dirs if present

--- CreateBoilerPlate.bat
+++ CreateBoilerPlate.bat
@@ -4,7 +4,10 @@ git add README.md
 git add .gitignore
 git commit -m"initial commit"
 cd external
+rmdir fmt
 git submodule add --depth 1 -- https://github.com/fmtlib/fmt.git
+rmdir spdlog
 git submodule add --depth 1 -- https://github.com/gabime/spdlog.git
+rmdir doctest
 git submodule add --depth 1 -- https://github.com/onqtam/doctest.git
 cd ..
