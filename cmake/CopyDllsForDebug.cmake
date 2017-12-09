993098830071477307cb65fbc78dfeab590d620e
Lectem <lectem@gmail.com>
2017-12-09 11:15:06 +0100

    RunFixupBundle was not working on OSX as expected, rename it for windows usage only

--- cmake/CopyDllsForDebug.cmake
+++ cmake/CopyDllsForDebug.cmake
@@ -3,7 +3,7 @@
 # the Windows platform. It allows generator expressions to be used to determine
 # the binary location
 #
-# Usage : run_fixup TARGET LIBS DIRS
+# Usage : copy_dlls_for_debug TARGET LIBS DIRS
 # - TARGET : A cmake target
 # - See fixup_bundle for LIBS and DIRS arguments
 
@@ -15,14 +15,15 @@ if(RUN_IT)
 else()
 
 set(THIS_FILE ${CMAKE_CURRENT_LIST_FILE})
-function(run_fixup _target _libs _dirs)
-	add_custom_command(
-		TARGET ${_target} POST_BUILD
-		COMMAND ${CMAKE_COMMAND} -DRUN_IT:BOOL=ON -DTO_FIXUP_FILE=$<TARGET_FILE:${_target}> -DTO_FIXUP_LIBS=${_libs} -DTO_FIXUP_DIRS=${_dirs} -P ${THIS_FILE}
-		COMMENT "Fixing up dependencies for ${_target}"
-		VERBATIM
-	)
-
+function(copy_dlls_for_debug _target _libs _dirs)
+	if(WIN32)
+		add_custom_command(
+			TARGET ${_target} POST_BUILD
+			COMMAND ${CMAKE_COMMAND} -DRUN_IT:BOOL=ON -DTO_FIXUP_FILE=$<TARGET_FILE:${_target}> -DTO_FIXUP_LIBS=${_libs} -DTO_FIXUP_DIRS=${_dirs} -P ${THIS_FILE}
+			COMMENT "Fixing up dependencies for ${_target}"
+			VERBATIM
+		)
+	endif(WIN32)
 endfunction()
 
 endif()
