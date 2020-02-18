0b579f527d7a139df1fe3c25c5243c35cc319c5c
Lectem <lectem@gmail.com>
2020-02-18 17:05:45 +0100

    Use pitchfork layout and remove variables for source files list (use add_* directly, or target_sources)

---
+++ source/foo-impl.h
@@ -0,0 +1,5 @@
+
+// This is where you would put implementation details of your library
+// It is recommended to only put public header files in the include/ folder, that is files that are required
+// to use your library. I suggest using the pitchfork layout which can be found here:
+// https://api.csswg.org/bikeshed/?force=1&url=https://raw.githubusercontent.com/vector-of-bool/pitchfork/develop/data/spec.bs
