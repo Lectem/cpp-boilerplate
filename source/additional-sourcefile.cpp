39441105fd7f9aa891585cbdc1b5bdbfbe6ede48
Lectem <lectem@gmail.com>
2020-02-18 17:23:29 +0100

    Showcase OS-based and optional source files

---
+++ source/additional-sourcefile.cpp
@@ -0,0 +1,6 @@
+
+// This file is only compiled if the cmake option BP_USE_ADDITIONAL_SOURCEFILE is set to ON
+
+#ifndef HAS_ADDITIONAL_SOURCEFILE
+#error If this file is compiled, the target must have HAS_ADDITIONAL_SOURCEFILE defined
+#endif
---
+++ source/windows-only.cpp
@@ -0,0 +1,5 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <Windows.h>
+
